3a30569ac18d9c2b8dc32b3cdb03bb955038635b
hortonc9810 <hortonc9810@student.faytechcc.edu>
2025-05-14 23:02:21 -0400

    Delete P1LAB1_HortonChelsea.py

--- P1LAB1_HortonChelsea.py
+++
@@ -1,9 +0,0 @@
-# Chelsea Horton
-# 4/1/2025
-# P1LAB1
-# Create a Python program using IDLE
-first_name = input('First Name?')
-last_name = input('Last Name?')
-print('Enter your first name:',first_name)
-print('Enter your last name:',last_name)
-print('Hello,',first_name,last_name,'! Welcome to CTI-110')
