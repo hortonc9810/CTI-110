793aa1f452a16ef37bc41763b7c08146f1db6f47
hortonc9810 <hortonc9810@student.faytechcc.edu>
2025-05-14 22:58:11 -0400

    Module 1 Assignments

---
+++ P1HW1_HortonChelsea.py
@@ -0,0 +1,21 @@
+# Chelsea Horton
+# 4/2/2025
+# P1HW1
+# Write a Python program that uses mathematical expressions
+
+print('-----Calculating Exponents----')
+print('\n')
+print('Enter an integer as the base value: ', end='')
+base_value = int(input())
+print('Enter an integer as the exponent: ', end='')
+exponent = int(input())
+print('\n')
+print(base_value,'raised to the power of', exponent, 'is',base_value**exponent,'!!' )
+print('\n')
+print('-----Addition and Subtraction----')
+print('\n')
+star_inte = int(input('Enter a starting integer: '))
+add_inte = int(input('Enter an integer to add:'))
+sub_inte = int(input('Enter an integer to subtract:'))
+print('\n')
+print(star_inte,'+',add_inte,'-', sub_inte,'is equal to',star_inte+add_inte-sub_inte)
